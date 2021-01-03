c87917e94af8a05e05edff2a0b0f70b51a62f199
Rodriguez1995 <Grodriguez1905@hotmail.com>
2021-01-02 23:38:06 -0500

    Add divice token to users (FCM)

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,4 @@
 <?php
-
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -29,5 +26,8 @@ Route::middleware(['auth:api'])->group(function () {
 	// appointments
 	Route::get('/appointments', 'AppointmentController@index');
 	Route::post('/appointments', 'AppointmentController@store');
+
+	// fcm
+	Route::post('/fcm/token', 'FirebaseController@postToken');
 
 });
